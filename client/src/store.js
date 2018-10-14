c63e8920c6b25c8cdbc04e73934dd78bb9afdf56
asm-morshed <mm.morshed28707@gmail.com>
2018-10-15 01:01:41 +0600

    remove redux tab viewer code

--- client/src/store.js
+++ client/src/store.js
@@ -7,8 +7,8 @@ const initialState = {}
 const store = createStore(
     rootReducer,
     initialState,
-    compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(applyMiddleware(thunk)
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
     )
 )
